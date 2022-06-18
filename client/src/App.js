70dee9ccd55846a43637401f2296caa8feafb62c
PETCH SRTP <srtp.th@gmail.com>
2022-06-18 19:18:22 +0700

    #update-banner-and-Search

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
 import { observer } from "mobx-react-lite";
-
+import Banner from "./components/indexPage/Banner"
 
 const App = observer(() => {
   return (
     <div>
-   
+   <Banner/>
     </div>
   );
 });
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,12 +2,13 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors:{
-        "light-blue":"#e8ebed",
-        "dark-blue":"#4065b0"
-  
-      }
-    
+      colors: {
+        "light-blue": "#e8ebed",
+        "dark-blue": "#4065b0",
+      },
+      backgroundImage: {
+        "index": "url('../public/images/food.jpg')",
+      },
     },
   },
   plugins: [require("daisyui")],
