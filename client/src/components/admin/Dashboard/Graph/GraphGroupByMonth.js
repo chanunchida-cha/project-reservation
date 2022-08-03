9a80c3a6be8d2f568ff18a0de3a3f3f8c52871e4
Yok <yok.8.12.2543@gmail.com>
2022-08-03 20:21:16 +0700

    finish v.1

--- client/src/components/admin/Dashboard/Graph/GraphGroupByMonth.js
+++ client/src/components/admin/Dashboard/Graph/GraphGroupByMonth.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
 import useSWR from "swr";
 import { useParams, useHistory } from "react-router-dom";
 import { Fragment, useState } from "react";
 import { Listbox, Transition } from "@headlessui/react";
 import { CheckIcon, SelectorIcon } from "@heroicons/react/solid";
+import { getPastelColor } from "pastel-color";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -15,7 +16,6 @@ import {
   Legend,
 } from "chart.js";
 import { Bar } from "react-chartjs-2";
-import faker from "faker";
 
 ChartJS.register(
   CategoryScale,
@@ -52,6 +52,8 @@ const selectYear = years1.concat(
   years2.filter((item) => years1.indexOf(item) < 0)
 );
 function GraphGroupByMonth() {
+  const thisMonth = new Date().getMonth();
+  const [value, setValue] = useState(month[thisMonth]);
   const [year, setYear] = useState(startYear);
   const {
     data: countGroupPartnerForMonth,
@@ -73,12 +75,19 @@ function GraphGroupByMonth() {
         position: "top",
       },
     },
+    scales: {
+      y: {
+        suggestedMin: 0,
+
+        suggestedMax: 100,
+        ticks: {
+          // forces step size to be 50 units
+          stepSize: 10,
+        },
+      },
+    },
   };
-  const labels = [];
-  const label = [];
-  // const labelsData = [];
-  let dataSets = [];
-  let datas = [];
+
   const sortLabelsYear = countGroupPartnerForMonth
     .slice()
     .sort((a, b) => a._id.year - b._id.year);
@@ -86,90 +95,132 @@ function GraphGroupByMonth() {
     .slice()
     .sort((a, b) => a._id.month - b._id.month);
 
-  datas = sortLabelsmonth.filter((label) => {
+  console.log(sortLabelsmonth);
+  const filterLabelsmonth = sortLabelsmonth.filter((label) => {
     let found = false;
 
-    if (Number(year) === label._id.year) {
+    if (
+      Number(value.key) === label._id.month &&
+      Number(year) === label._id.year
+    ) {
       found = true;
     }
 
     return found;
   });
 
-  // const duplicateIds = datas
-  //   .map((data) => data._id.month)
-  //   .filter((data, i, dataIds) => dataIds.indexOf(data) !== i);
+  const groupLabel = filterLabelsmonth.reduce((group, data) => {
+    const { month, year } = data._id;
+    const key = `${month}/${year}`;
+    group[key] = group[key] ?? [];
+    group[key].push(data);
+    return group;
+  }, {});
 
-  //   console.log(duplicateIds);
+  const groupByCategory = filterLabelsmonth.reduce((group, data) => {
+    const { restaurantName } = data.information;
+    group[restaurantName] = group[restaurantName] ?? [];
+    group[restaurantName].push(data.count);
+    return group;
+  }, {});
 
-//   const values = [{
-//     id: 10,
-//     name: 'someName1'
-//   },
-//   {
-//     id: 10,
-//     name: 'someName2'
-//   },
-//   {
-//     id: 10,
-//     name: 'someName2'
-//   },
-//   {
-//     id: 11,
-//     name: 'someName3'
-//   },
-//   {
-//     id: 12,
-//     name: 'someName4'
-//   }
-// ];
+  const labels = Object.keys(groupLabel);
+  console.log("groupByCategory", groupByCategory);
+  const dataSets = [];
+  console.log(getPastelColor());
+  Object.keys(groupByCategory).forEach((key) =>
+    dataSets.push({
+      label: key,
+      data: groupByCategory[key],
+      backgroundColor: "#" + Math.floor(Math.random() * 16777215).toString(16),
+    })
+  );
 
-// const duplicateIds = values
-//   .map(v => v.id)
-//   .filter((v, i, vIds) => vIds.indexOf(v) !== i)
-//   console.log(duplicateIds);
-// const duplicates = values
-//   .filter(obj => duplicateIds.includes(obj.id));
-// console.log(duplicates)
-// const duplicates = datas.filter((data)=>{duplicateIds.inCludes(data._id.month)})
-  // for(const theMonth of month){
-  //   for(const count of countGroupPartnerForMonth){
-  //     if(theMonth.key === count._id.month){
-  //       datas.push({
-
-  //       })
-
-  //     }
-  //   }
-  // }
-
-  // labelsData.push(`ร้าน${resName.restaurantName}`);
-  //     labels.push(`เดือน ${count._id.month} ปี ${count._id.year}`);
-
-  // for (const count of datas) {
-  //   for (const resName of count.information) {
-  //     labels.push(`เดือน ${count._id.month} ปี ${count._id.year}`);
-  //     label.push(`เดือน ${count._id.month} ปี ${count._id.year}`);
-  //     dataSets.push({
-  //       label: `ร้าน${resName.restaurantName}`,
-  //       data: count.count,
-  //     });
-  //   }
-
-  console.log(datas);
+  console.log(dataSets);
   const data = {
-    labels: ["a,", "b"],
-    datasets: [
-      {
-        label: "A",
-        data: [1, 2],
-      },
-    ],
+    labels: labels,
+    datasets: [...dataSets],
   };
 
   return (
     <div>
       <div className="grid grid-cols-10 gap-4">
+        <div className="col-span-3">
+          <Listbox value={value} onChange={setValue}>
+            {({ open }) => (
+              <>
+                <Listbox.Label className="block text-sm font-medium text-gray-700">
+                  เลือกเดือนที่ต้องการ
+                </Listbox.Label>
+                <div className="mt-1 relative">
+                  <Listbox.Button className="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                    <span className="flex items-center">{value.i18n}</span>
+                    <span className="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                      <SelectorIcon
+                        className="h-5 w-5 text-gray-400"
+                        aria-hidden="true"
+                      />
+                    </span>
+                  </Listbox.Button>
+
+                  <Transition
+                    show={open}
+                    as={Fragment}
+                    leave="transition ease-in duration-100"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                    <Listbox.Options className="absolute z-10 mt-1 w-80 bg-white shadow-lg max-h-56 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
+                      {month.map((month, index) => (
+                        <Listbox.Option
+                          key={index}
+                          className={({ active }) =>
+                            classNames(
+                              active
+                                ? "text-white bg-[#189bff] "
+                                : " text-gray-900",
+                              "cursor-default select-none relative py-2 pl-3 pr-9"
+                            )
+                          }
+                          value={month}
+                        >
+                          {({ selected, active }) => (
+                            <>
+                              <div className="flex items-center">
+                                <span
+                                  className={classNames(
+                                    selected ? "font-semibold" : "font-normal",
+                                    "ml-3 block truncate"
+                                  )}
+                                >
+                                  {month.i18n}
+                                </span>
+                              </div>
+
+                              {selected ? (
+                                <span
+                                  className={classNames(
+                                    active ? "text-white" : "text--[#189bff] ",
+                                    "absolute inset-y-0 right-0 flex items-center pr-4"
+                                  )}
+                                >
+                                  <CheckIcon
+                                    className="h-5 w-5"
+                                    aria-hidden="true"
+                                  />
+                                </span>
+                              ) : null}
+                            </>
+                          )}
+                        </Listbox.Option>
+                      ))}
+                    </Listbox.Options>
+                  </Transition>
+                </div>
+              </>
+            )}
+          </Listbox>
+        </div>
         <div className="col-span-3">
           <Listbox value={year} onChange={setYear}>
             {({ open }) => (
